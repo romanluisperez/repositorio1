3b0741d8307afe7081a735a89aae8588633db5ef
Roman Perez <romanperez1@gmail.com>
2021-10-27 19:10:37 -0300

    Agregamos una carpeta y dos archivos py

---
+++ bucle.py
@@ -0,0 +1,20 @@
+
+
+def run():
+    LIMITE = 1000  #el resultado no podra superar este valor 
+    
+    contador = 0 
+    potencia_2 = 2**contador
+    
+    while potencia_2 < LIMITE:
+        contador = contador + 1 
+        potencia_2 = 2**contador
+    
+        print ('2 elevado a ' + str(contador) + ' ea igual a -> ' + str(potencia_2))
+    
+    
+    
+    
+if __name__ == "__main__":
+        run()
+    
---
+++ python-repositorio/conjuntos.py
@@ -0,0 +1,9 @@
+#Teoria de conjuntos
+A={1,2,3,4,5}
+B={4,5,6,0}
+C={4,6,7,8,5}
+print (A==B)
+print (A | B) #equivale a un or
+print (A & B) #equivale a un and
+print (A - B)
+print (A ^ C) #diferencia simetrica es decir quedan afuera los elementos comunes en ambos conj
