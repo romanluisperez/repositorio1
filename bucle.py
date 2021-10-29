36e0bebd8d2d7b6d8f5f08a124df72acb80ea4b9
Roman Perez <romanperez1@gmail.com>
2021-10-29 17:52:46 -0300

    rama1

--- bucle.py
+++ bucle.py
@@ -1,5 +1,5 @@
 
-rama1
+#rama1
 def run():
     LIMITE = 1000  #el resultado no podra superar este valor 
     contador_rama=0
@@ -17,4 +17,5 @@ def run():
     
 if __name__ == "__main__":
         run()
-    
+
+print('rama1')   
